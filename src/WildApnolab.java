d03b333a76bbd9d3856402a37f118551e6cb951c
Leo THEODON <leo.theodon@gmail.com>
2020-11-21 13:56:13 +0100

    working P1
    go check WildApnolab.java

--- src/WildApnolab.java
+++ src/WildApnolab.java
@@ -7,14 +7,25 @@ import org.megadix.jfcm.conn.WeightedConnection;
 import org.megadix.jfcm.utils.FcmIO;
 import org.megadix.jfcm.utils.FcmRunner;
 import org.megadix.jfcm.utils.SimpleFcmRunner;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 
 
@@ -37,156 +48,154 @@ public class WildApnolab {
 			System.out.println("Faillure");
 			e.printStackTrace();
 		}
-        map = new CognitiveMap("Investments");
+        map = new CognitiveMap("WildApnolab");
         map = test.get(0);
-        runner = new SimpleFcmRunner(map, 0.1, 1000);
+        runner = new SimpleFcmRunner(map, 0.01, 100000);
         nf = NumberFormat.getNumberInstance(Locale.ENGLISH);
         nf.setMaximumFractionDigits(8);
     }
 
-    /**
-     * Keep "Rain" and "Rangers" with fixed outputs of 0.1
-     */
-    public void test_scenario_1() throws Exception {
-        resetMap();
+    
 
-        // FIXED VALUES
-        map.getConcepts().get("Rain").setOutput(0.1);
-        map.getConcepts().get("Rain").setFixedOutput(true);
+    
+    
+    public void test_scenario_1(double rain, double rangers) throws Exception {
+        
+        
+       	Map<String, Double> rminList = new HashMap<String, Double>();
+    	Map<String, Double> rmaxList = new HashMap<String, Double>();
+    	Map<String, Double> stepList = new HashMap<String, Double>();
+    	Map<String, Double> costList = new HashMap<String, Double>();
+    	Map<String, Integer> interList = new HashMap<String, Integer>();
+    	Map<String, Double> paramList = new HashMap<String, Double>();
+    	List<String> nameList = new ArrayList<String>();
+    	List<String> costNameList = new ArrayList<String>();
+        
+        try {
+            File fXmlFile = new File("Wild_try.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
 
-        map.getConcepts().get("Rangers").setOutput(0.1);
-        map.getConcepts().get("Rangers").setFixedOutput(true);
-        // start from zero
-        map.getConcepts().get("Grassland").setOutput(0.0);
+            NodeList nList = doc.getElementsByTagName("concept");
+            NodeList coList = doc.getElementsByTagName("cost");
+        	
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String name = eElement.getAttribute("name");
+                    Double rmin = Double.parseDouble(eElement.getElementsByTagName("rmin").item(0).getTextContent());
+                    Double rmax = Double.parseDouble(eElement.getElementsByTagName("rmax").item(0).getTextContent());
+                    Double step = Double.parseDouble(eElement.getElementsByTagName("step").item(0).getTextContent());
+                    rminList.put(name, rmin);
+                    rmaxList.put(name, rmax);
+                    stepList.put(name, step);
+                    nameList.add(name);
+	                }
+	            }
+            
+            for (int temp = 0; temp < coList.getLength(); temp++) {
+                Node nNode = coList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String name = eElement.getAttribute("name");
+                    Double cost = Double.parseDouble(eElement.getElementsByTagName("weight").item(0).getTextContent());
+                    costList.put(name, cost);
+                    costNameList.add(name);
+	                }
+	            }
+            
+            } catch (Exception e) {
+            e.printStackTrace();
+          }
+        
+        int nb_iter = 1;
+        int nb_inter = 0;
+        
+        for (String name : nameList) {
+        	Double rmin = rminList.get(name);
+        	Double rmax = rmaxList.get(name);
+        	Double step = stepList.get(name);
+        	nb_inter = (int) (step/100+(rmax - rmin) / step)+1; 
+        	interList.put(name, nb_inter);
+        	nb_iter *= nb_inter;
+            //System.out.print(nb_inter + "\n");
+        }
+        
+        
+        
+        // Global Iteration
+        double top_resultat = -99999.0;
+    	Map<String, Double> bestParamList = new HashMap<String, Double>();
+        for (String name : nameList) {
+        	bestParamList.put(name, 0.0);
+        }
+        for (int i=0; i<nb_iter; i++) {
+        	int tmp_tot = 1;
+        	for (String name : nameList) {
+        		int r = i/tmp_tot % interList.get(name);
+        		paramList.put(name,rminList.get(name) + r*stepList.get(name));
+        		tmp_tot *= interList.get(name);
+        	}
+        	
+        	// DO THE MAP!!§§§
+            resetMap();
+            map.getConcepts().get("Rain").setOutput(0.1);
+            map.getConcepts().get("Rangers").setOutput(0.1);
+            map.getConcepts().get("Grassland").setOutput(0.5);
+            map.getConcepts().get("Poachers").setOutput(0.1);
+            map.getConcepts().get("Herbivores").setOutput(0.2);
+            map.getConcepts().get("Predators").setOutput(0.1);
+            
 
-        map.getConcepts().get("Poachers").setOutput(0.0);
+            // FIXED VALUES
+            for (String name : nameList) {
+            	map.getConcepts().get(name).setOutput(paramList.get(name));
+                map.getConcepts().get(name).setFixedOutput(true);
+            	//System.out.print(name + ":" + paramList.get(name) + "\n");
+            }
+            
 
-        map.getConcepts().get("Erbivores").setOutput(0.0);
-
-        map.getConcepts().get("Predators").setOutput(0.0);
-
-        showResults("Scenario 1", runner.converge());
+            boolean b = runner.converge();
+        	//System.out.print("Convergence:" + b + "\n");
+        	
+        	double resultat = 0.0;
+            for (String name : costNameList) {
+            	//System.out.print("name:" + name + "\n");
+            	Double out = map.getConcepts().get(name).getOutput();
+            	resultat += costList.get(name)*out;
+            	//System.out.print("resultat: " + resultat + "\n");
+            }
+            if (b & (resultat > top_resultat)){
+            	top_resultat = resultat;
+            	for (String name : nameList) {
+                	bestParamList.put(name, paramList.get(name));
+                }
+            }
+        	
+        	
+            
+            //Double nb = map.getConcepts().get("Herbivores").getOutput();
+        	//System.out.print("Herbivores:" + nb + "\n");
+        	//System.out.print("resultat:" + resultat + "\n");
+        }
+        
+        System.out.print("Meilleurs résultats: " + top_resultat + "\n");
+        for (String name : nameList) {
+        	System.out.print(name + ":" + bestParamList.get(name) + "\n");
+        }
 
     }
 
-    /**
-     * Keep "Rain" and "Rangers" with fixed outputs of 0.1 and 0.5
-     */
-    public void test_scenario_2() throws Exception {
-        resetMap();
-
-        // FIXED VALUES
-        map.getConcepts().get("Rain").setOutput(0.1);
-        map.getConcepts().get("Rain").setFixedOutput(true);
-        map.getConcepts().get("Rangers").setOutput(0.5);
-        map.getConcepts().get("Rangers").setFixedOutput(true);
-        // start from zero
-        map.getConcepts().get("Grassland").setOutput(0.0);
-        map.getConcepts().get("Poachers").setOutput(0.0);
-        map.getConcepts().get("Erbivores").setOutput(0.0);
-        map.getConcepts().get("Predators").setOutput(0.0);
-
-        showResults("Scenario 2", runner.converge());
-
-    }
-
-    /**
-     * Keep "Rain" and "Rangers" with fixed outputs of 0.1 and 0.9
-     */
-    public void test_scenario_3() throws Exception {
-        resetMap();
-
-        // FIXED VALUES
-        map.getConcepts().get("Rain").setOutput(0.1);
-        map.getConcepts().get("Rain").setFixedOutput(true);
-        map.getConcepts().get("Rangers").setOutput(0.9);
-        map.getConcepts().get("Rangers").setFixedOutput(true);
-        // start from zero
-        map.getConcepts().get("Grassland").setOutput(0.0);
-        map.getConcepts().get("Poachers").setOutput(0.0);
-        map.getConcepts().get("Erbivores").setOutput(0.0);
-        map.getConcepts().get("Predators").setOutput(0.0);
-
-        showResults("Scenario 3", runner.converge());
-
-    }
-
-
-    /**
-     * Keep "Rain" and "Rangers" with fixed outputs of 0.5 and 0.1
-     */
-    public void test_scenario_4() throws Exception {
-        resetMap();
-
-        // FIXED VALUES
-        map.getConcepts().get("Rain").setOutput(0.5);
-        map.getConcepts().get("Rain").setFixedOutput(true);
-        map.getConcepts().get("Rangers").setOutput(0.1);
-        map.getConcepts().get("Rangers").setFixedOutput(true);
-        // start from zero
-        map.getConcepts().get("Grassland").setOutput(0.0);
-        map.getConcepts().get("Poachers").setOutput(0.0);
-        map.getConcepts().get("Erbivores").setOutput(0.0);
-        map.getConcepts().get("Predators").setOutput(0.0);
-
-        showResults("Scenario 4", runner.converge());
-
-    }
-
-
-    /**
-     * Keep "Rain" and "Rangers" with fixed outputs of 0.5 and 0.5
-     */
-    public void test_scenario_5() throws Exception {
-        resetMap();
-
-        // FIXED VALUES
-        map.getConcepts().get("Rain").setOutput(0.5);
-        map.getConcepts().get("Rain").setFixedOutput(true);
-        map.getConcepts().get("Rangers").setOutput(0.5);
-        map.getConcepts().get("Rangers").setFixedOutput(true);
-        // start from zero
-        map.getConcepts().get("Grassland").setOutput(0.0);
-        map.getConcepts().get("Poachers").setOutput(0.0);
-        map.getConcepts().get("Erbivores").setOutput(0.0);
-        map.getConcepts().get("Predators").setOutput(0.0);
-
-        showResults("Scenario 5", runner.converge());
-
-    }
-
-    /**
-     * Keep "Rain" and "Rangers" with fixed outputs of 0.5 and 0.9
-     */
-    public void test_scenario_6() throws Exception {
-        resetMap();
-
-        // FIXED VALUES
-        map.getConcepts().get("Rain").setOutput(0.5);
-        map.getConcepts().get("Rain").setFixedOutput(true);
-        map.getConcepts().get("Rangers").setOutput(0.9);
-        map.getConcepts().get("Rangers").setFixedOutput(true);
-        // start from zero
-        map.getConcepts().get("Grassland").setOutput(0.0);
-        map.getConcepts().get("Poachers").setOutput(0.0);
-        map.getConcepts().get("Erbivores").setOutput(0.0);
-        map.getConcepts().get("Predators").setOutput(0.0);
-
-        showResults("Scenario 6", runner.converge());
-
-    }
 
     public void run() {
         try {
-            System.out.print("Scenario\tConverged\t");
-            ExampleUtils.printMapHeader(map, "\t");
+            //System.out.print("Scenario\tConverged\t");
+            //ExampleUtils.printMapHeader(map, "\t");
 
-            test_scenario_1();
-            test_scenario_2();
-            test_scenario_3();
-            test_scenario_4();
-            test_scenario_5();
-            test_scenario_6();
+            test_scenario_1(0.1,0.1);
 
         } catch (Exception e) {
             // TODO Auto-generated catch block
@@ -205,8 +214,8 @@ public class WildApnolab {
     }
 
     void showResults(String scenario, boolean converged) {
-        System.out.print(scenario + "\t" + converged + "\t");
-        ExampleUtils.printMapState(map, "\t", nf);
+        //System.out.print(scenario + "\t" + converged + "\t");
+        //ExampleUtils.printMapState(map, "\t", nf);
     }
 
     public static void main(String[] args) {
